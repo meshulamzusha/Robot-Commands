e79e8b6c1d4fab690901fd0b3784191f53d7fd57
meshulamzusha <nauko763@gmail.com>
2025-11-03 22:54:07 +0200

    fix: refactor robot_action method for improved command handling and error management

--- classes/guard_robot.py
+++ classes/guard_robot.py
@@ -8,21 +8,36 @@ class GuardRobot(SpeakableMixin):
         self.is_on = True
 
 
-    def action(self):
-        while self.is_on:
+    def robot_action(self):
             complete_command = super()._get_command(self)
             command = complete_command[0]
             parm = complete_command[1]
 
             match command:
                 case 'SAY':
-                    super().speak(parm)
+                    try:
+                        super().speak(parm)
+                    except AttributeError:
+                        print(f'unsupported command {command}')
                 case 'MOVE':
-                    if callable(self.move)
+                    new_position = parm.split()
+
+                    try:
+                        x = int(new_position[0])
+                        y = int(new_position[1])
+                    except (ValueError, IndexError):
+                        print('For moving enter your command like this MOVE <x> <Y>')
+                    else:
+                        try:
+                            super().move(x, y)
+                        except AttributeError:
+                            print(f'unsupported command {command}')
                 case 'WHERE':
-                    print(f'')
+                    try:
+                        super().print_position()
+                    except AttributeError:
+                        print(f'unsupported command {command}')
                 case 'EXIT':
-                    self.is_on = False
                     sys.exit()
                 case _:
                     print(f'command not found')
